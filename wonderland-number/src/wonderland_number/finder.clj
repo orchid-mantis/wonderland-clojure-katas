b9359a8833e70507225d15166f1a0abffa035a1d
regul <regul@mantis>
2020-02-20 10:54:14 +0000

    Add wonderland-number impl

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,16 @@
 (ns wonderland-number.finder)
 
+(defn sameDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [wondernum? #(and (sameDigits? (* 2 %) %)
+                         (sameDigits? (* 3 %) %)
+                         (sameDigits? (* 4 %) %)
+                         (sameDigits? (* 5 %) %)
+                         (sameDigits? (* 6 %) %))]
+    (->> (range 100000 1000000)
+         (filter wondernum?)
+         (first))))
