6b620cf5874fcd4e7edfe11a73a70a81df2b0c01
regul <regul@mantis>
2020-03-03 22:41:23 +0000

    Add doublets solution

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -1,11 +1,46 @@
 (ns doublets.solver
-  (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+  (:require [clojure.java.io :as io]))
 
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
                (read-string)))
 
+(defn all-pairs [dict]
+  (for [w1 dict
+        w2 dict]
+    [w1 w2]))
+
+(defn one-char-diff? [w1 w2]
+  (->> (map (fn [ch1 ch2] (= ch1 ch2)) w1 w2)
+       (filter false?)
+       (count)
+       (= 1)))
+
+(defn paths-by-word [dict]
+  (let [pairs (->> (all-pairs dict)
+                   (filter (fn [[w1 w2]] (one-char-diff? w1 w2))))
+        maps (for [w dict]
+               (assoc {} w (->> (filter (fn [[first _]] (= first w)) pairs)
+                                (map second)
+                                (set))))]
+    (apply merge maps)))
+
+(defn gen-paths [start-word end-word paths dict]
+  (let [next-words (get dict start-word)]
+    (cond
+      (contains? next-words end-word) (conj (conj paths start-word) end-word)
+      :else (->> (vec next-words)
+                 (filter (fn [w] (not-any? #(= w %) paths)))
+                 (map #(gen-paths % end-word (conj paths start-word) dict))
+                 (first)
+                 (vec)))))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (let [w1-size (count word1)]
+    (cond
+      (not= w1-size (count word2)) []
+      :else (->> words
+                 (filter #(= w1-size (count %)))
+                 (paths-by-word)
+                 (gen-paths word1 word2 [])))))
--- doublets/test/doublets/solver_test.clj
+++ doublets/test/doublets/solver_test.clj
@@ -1,6 +1,32 @@
 (ns doublets.solver-test
-  (:require [clojure.test :refer :all]
-            [doublets.solver :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [doublets.solver :refer [one-char-diff? paths-by-word doublets]]))
+
+(deftest test-one-char-diff?
+  (testing "no char difference"
+    (is (= false
+           (one-char-diff? "frog" "frog"))))
+  (testing "single char difference"
+    (is (= true
+           (one-char-diff? "look" "cook"))))
+  (testing "multiple char difference"
+    (is (= false
+           (one-char-diff? "frog" "lock")))))
+
+(def small-dict ["door"
+                 "boor"
+                 "book"
+                 "look"
+                 "frog"])
+
+(deftest test-paths-by-word
+  (testing "can generate correct paths for words of same size"
+    (is (= {"door" #{"boor"}
+            "boor" #{"book" "door"}
+            "book" #{"look" "boor"}
+            "look" #{"book"}
+            "frog" #{}}
+           (paths-by-word small-dict)))))
 
 (deftest solver-test
   (testing "with word links found"
