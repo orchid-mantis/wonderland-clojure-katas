01d0c60b2a37e0276500e96c68d0b39136ec0bfc
regul <regul@mantis>
2020-03-06 21:01:03 +0000

    Add test-refresh plugin

--- card-game-war/project.clj
+++ card-game-war/project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :main card-game-war.game)
+  :main card-game-war.game
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}})
