4d1fedcb33261c5f3dd04a7f85ebf0d5ccb536d9
regul <regul@mantis>
2020-03-06 19:10:22 +0000

    Add test-refresh plugin

--- tiny-maze/project.clj
+++ tiny-maze/project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :main tiny-maze.solver)
+  :main tiny-maze.solver
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}})
