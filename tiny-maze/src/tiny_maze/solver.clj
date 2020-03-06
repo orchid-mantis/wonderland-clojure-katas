25d3a47431540cd04c9dfe8cff1f922c211f67d0
regul <regul@mantis>
2020-03-06 19:21:06 +0000

    Add tiny-maze solution

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,29 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(defn next-moves [m [x y]]
+  (let [left-pos  [(dec x) y]
+        upper-pos [x (dec y)]
+        right-pos [(inc x) y]
+        lower-pos [x (inc y)]
+        neighbours [[left-pos (get-in m left-pos)]
+                    [upper-pos (get-in m upper-pos)]
+                    [right-pos (get-in m right-pos)]
+                    [lower-pos (get-in m lower-pos)]]]
+    (->> neighbours
+         (filter (fn [[_ val]] (or (= val 0) (= val :E))))
+         (map first))))
+
+(defn gen-path [m path [x y]]
+  (cond
+    (= :E (get-in m [x y])) (conj path [x y])
+    :else (->> (next-moves m [x y])
+               (filter (fn [next] (not-any? #(= next %) path)))
+               (map (fn [next] (gen-path m (conj path [x y]) next)))
+               (filter some?)
+               (first))))
+
+(defn solve-maze [maze]
+  (let [path (gen-path maze [] [0 0])
+        new-maze (->> (map (fn [pos] [pos maze]) path)
+                      (reduce (fn [[pos1 m1] [pos2 _]] [pos2 (assoc-in m1 pos1 :x)])))]
+    (assoc-in (second new-maze) (first new-maze) :x)))
--- tiny-maze/test/tiny_maze/solver_test.clj
+++ tiny-maze/test/tiny_maze/solver_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer [deftest testing is]]
             [tiny-maze.solver :as solver]))
 
+(def maze [[:S 0 1]
+           [1  0 1]
+           [1  0 :E]])
+
+(deftest test-next-moves
+  (testing "can generate all possible moves from current position in maze"
+    (is (= [[0 1]]
+           (solver/next-moves maze [0 0])))
+    (is (= [[0 1] [2 1]]
+           (solver/next-moves maze [1 1]))))
+  (testing "end position (marked with :E) is accessible from position [2 1]"
+    (is (= [[1 1] [2 2]]
+           (solver/next-moves maze [2 1])))))
+
 (deftest test-solve-maze
   (testing "can find way to exit with 3x3 maze"
     (let [maze [[:S 0 1]
