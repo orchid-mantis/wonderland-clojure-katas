6e76864e2649911281e5c93dd19c35f55b00375a
regul <regul@mantis>
2020-03-06 19:43:08 +0000

    Refactor: Reduce gen-path args & rename

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -7,15 +7,16 @@
         [x (inc y)]]
        (filter #(or (= (get-in m %) 0) (= (get-in m %) :E)))))
 
-(defn gen-path [m path [x y]]
-  (cond
-    (= :E (get-in m [x y])) (conj path [x y])
-    :else (->> (next-moves m [x y])
-               (filter (fn [next] (not-any? #(= next %) path)))
-               (map (fn [next] (gen-path m (conj path [x y]) next)))
-               (filter some?)
-               (first))))
+(defn gen-path [maze path]
+  (let [[x y] (last path)]
+    (cond
+      (= :E (get-in maze [x y])) (conj path [x y])
+      :else (->> (next-moves maze [x y])
+                 (filter (fn [move] (not-any? #(= move %) path)))
+                 (map (fn [move] (gen-path maze (conj path move))))
+                 (filter some?)
+                 (first)))))
 
 (defn solve-maze [maze]
-  (let [path (gen-path maze [] [0 0])]
+  (let [path (gen-path maze [[0 0]])]
     (reduce #(assoc-in %1 %2 :x) maze path)))
