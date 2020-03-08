0776a8274f660ace4b26e83d985c413abd14b151
regul <regul@mantis>
2020-03-08 14:46:04 +0000

    Add card-game-war solution

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,51 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(def shuffled-decks
+  (partition 26 (shuffle cards)))
 
-(defn play-game [player1-cards player2-cards])
+(def player1-cards (first shuffled-decks))
+(def player2-cards (second shuffled-decks))
+
+(def ranks-int (->> (map-indexed (fn [i r] {r i}) ranks)
+                    (reduce #(merge %1 %2))))
+
+(defn rank-val [[_ r]]
+  (get ranks-int r))
+
+(defn play-round [[p1-first & p1-rest] [p2-first & p2-rest] prize]
+  (let [p1-rank (rank-val p1-first)
+        p2-rank (rank-val p2-first)
+        final-prize (shuffle (concat prize [p1-first p2-first]))]
+    (cond
+      (or (nil? p1-first) (nil? p2-first)) []
+      (> p1-rank p2-rank) [[[:p1-won p1-first p2-first]
+                            (concat p1-rest final-prize)
+                            p2-rest]]
+
+      (< p1-rank p2-rank) [[[:p2-won p1-first p2-first]
+                            p1-rest
+                            (concat p2-rest final-prize)]]
+
+      :else (let [p1-deck (drop 3 p1-rest)
+                  p2-deck (drop 3 p2-rest)
+                  prize (concat prize
+                                [p1-first p2-first]
+                                (take 3 p1-rest)
+                                (take 3 p2-rest))]
+              (concat [[[:tie p1-first p2-first] p1-deck p2-deck]]
+                      (play-round p1-deck p2-deck prize))))))
+
+(defn game-loop [steps]
+  (let [step (last steps)
+        [_ p1-deck p2-deck] step]
+    (cond
+      ;(= (count steps) 10) steps
+      (and (empty? p1-deck) (empty? p2-deck)) {:game-result :tie :steps steps}
+      (empty? p1-deck) {:game-result :player2-won :steps steps}
+      (empty? p2-deck) {:game-result :player1-won :steps steps}
+      :else (recur (concat steps (play-round p1-deck p2-deck []))))
+    ))
+
+(defn play-game [p1-deck p2-deck]
+  (game-loop [[[] p1-deck p2-deck]]))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -1,16 +1,128 @@
 (ns card-game-war.game-test
-  (:require [clojure.test :refer :all]
-            [card-game-war.game :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [card-game-war.game :as g]))
 
+(deftest test-rank-val
+  (testing "10s are higher rank than 2s"
+    (is (> (g/rank-val [:diamond 10])
+           (g/rank-val [:heart 2]))))
+  (testing "jacks are higher rank than 10s"
+    (is (> (g/rank-val [:diamond :jack])
+           (g/rank-val [:heart 10]))))
+  (testing "queens are higher rank than jacks"
+    (is (> (g/rank-val [:diamond :queen])
+           (g/rank-val [:heart :jack]))))
+  (testing "kings are higher rank than queens"
+    (is (> (g/rank-val [:diamond :king])
+           (g/rank-val [:heart :queen]))))
+  (testing "aces are higher rank than kings"
+    (is (> (g/rank-val [:diamond :ace])
+           (g/rank-val [:heart :king])))))
 
-;; fill in  tests for your game
 (deftest test-play-round
+  (testing "returns [] when one or both players have empty deck"
+    (is (= []
+           (g/play-round nil nil [])))
+    (is (= []
+           (g/play-round [[:diamond :ace]] nil []))))
+
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings"))
+    (let [[[result p1-deck p2-deck]] (g/play-round [[:diamond :ace]] [[:heart 10]] [])]
+      (is (= [:p1-won [:diamond :ace] [:heart 10]]
+             result))
+      (is (= #{[:diamond :ace] [:heart 10]}
+             (set p1-deck)))
+      (is (nil? p2-deck)))
+
+    (let [[[result p1-deck p2-deck]] (g/play-round [[:diamond 5]] [[:heart 10]] [])]
+      (is (= [:p2-won [:diamond 5] [:heart 10]]
+             result))
+      (is (= #{[:diamond 5] [:heart 10]}
+             (set p2-deck)))
+      (is (nil? p1-deck))))
+
+  (testing "tie scenario where both players run out of cards"
+    (let [round (g/play-round [[:diamond :ace]] [[:heart :ace]] [])
+          [result p1-deck p2-deck] (first round)]
+      (is (= result
+             [:tie [:diamond :ace] [:heart :ace]]))
+      (is (empty? p1-deck))
+      (is (empty? p2-deck)))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "first step should contain initial decks"
+    (let [p1-deck '([:c :jack] [:c 2] [:c 10])
+          p2-deck '([:c 5] [:c 6] [:c 6])
+          {:keys [steps]} (g/play-game p1-deck p2-deck)
+          [_ deck1 deck2] (first steps)]
+      (is (> (count steps)
+             1))
+      (is (= deck1
+             p1-deck))
+      (is (= deck2
+             p2-deck))))
 
+  (testing "the player loses when they run out of cards"
+    (let [p1-deck '([:c :jack] [:c :ace] [:c 10])
+          p2-deck '([:c 10] [:c :queen] [:c 6])
+          {:keys [game-result steps]} (g/play-game p1-deck p2-deck)
+          [_ _ p2-deck] (last steps)]
+      (is (some? steps))
+      (is (= :player1-won game-result))
+      (is (empty? p2-deck))))
+
+  (testing "can generate correct game steps"
+    (let [p1-deck '([:c :jack] [:c 10] [:c 9] [:c 6] [:c :ace])
+          p2-deck '([:c 10] [:c :ace] [:c 6] [:c 2] [:c 9])
+          {:keys [steps]} (g/play-game p1-deck p2-deck)
+          steps (map first steps)]
+      (is (= '([]
+               [:p1-won [:c :jack] [:c 10]]
+               [:p2-won [:c 10] [:c :ace]]
+               [:p1-won [:c 9] [:c 6]])
+             (take 4 steps)))))
+
+  (testing "tie in first round"
+    (let [three-cards (repeat 3 [:c 2])
+          p1-deck (concat '([:c :jack]) three-cards '([:c :ace]))
+          p2-deck (concat '([:c :jack]) three-cards '([:c 9]))
+          {:keys [game-result steps]} (g/play-game p1-deck p2-deck)
+          step-result (map first steps)]
+      (is (= :player1-won
+             game-result))
+      (is (= '([]
+               [:tie [:c :jack] [:c :jack]]
+               [:p1-won [:c :ace] [:c 9]])
+             step-result))))
+
+  (testing "double tie in first round"
+    (let [three-cards (repeat 3 [:c 2])
+          p1-deck (concat '([:c :jack]) three-cards '([:c :ace]) three-cards '([:c 10]))
+          p2-deck (concat '([:c :jack]) three-cards '([:c :ace]) three-cards '([:c 2]))
+          {:keys [game-result steps]} (g/play-game p1-deck p2-deck)
+          step-result (map first steps)
+          [_ deck1 _] (last steps)]
+      (is (= :player1-won
+             game-result))
+      (is (= '([]
+               [:tie [:c :jack] [:c :jack]]
+               [:tie [:c :ace] [:c :ace]]
+               [:p1-won [:c 10] [:c 2]])
+             step-result))
+      (is (= (+ (count p1-deck) (count p2-deck))
+             (count deck1)))))
+
+  (testing "can play with all cards"
+    (let [{:keys [game-result]} (g/play-game g/player1-cards g/player2-cards)]
+      (is (or (= :player1-won
+                 game-result)
+              (= :player2-won
+                 game-result)
+              (= :tie
+                 game-result)))))
+
+  (testing "should be tie when players have same decks"
+    (let [deck g/player1-cards
+          {:keys [game-result]} (g/play-game deck deck)]
+      (is (= :tie
+             game-result)))))
